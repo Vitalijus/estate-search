ffb82f0c6845c5c7d075d5785867ef82dcc65fd6
Vitalijus Desukas <vitalij.desuk@gmail.com>
2024-08-05 19:04:01 +0300

    add vcr cassettes

--- spec/services/dog_api/get_breed_spec.rb
+++ spec/services/dog_api/get_breed_spec.rb
@@ -7,17 +7,21 @@ describe DogApi::GetBreed, type: :request do
 
     subject { DogApi::GetBreed }
 
-    it "should return list of images" do
-      dog_api = subject.new(breed)
+    it "should return list of images", vcr: { re_record_interval: 30.days } do
+      VCR.use_cassette("dog_api/breed_images") do
+        dog_api = subject.new(breed)
 
-      expect(dog_api.build_result).not_to be_empty
+        expect(dog_api.build_result).not_to be_empty
+      end
     end
 
-    it "should return an error message" do
-      dog_api = subject.new("Maine Coon")
-      dog_api.build_result
+    it "should return an error message", vcr: { re_record_interval: 30.days } do
+      VCR.use_cassette("dog_api/error_message") do
+        dog_api = subject.new("Maine Coon")
+        dog_api.build_result
 
-      expect(dog_api.error).to eq("Unsuccessful response, couldn't get maine coon images")
+        expect(dog_api.error).to eq("Unsuccessful response, couldn't get maine coon images")
+      end
     end
   end
 end
